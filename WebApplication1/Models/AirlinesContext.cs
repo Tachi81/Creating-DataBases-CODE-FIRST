aa36ba1ff8d29babe937acfb3f283d765da8f018
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-11 00:16:10 +0100

    Creating simple database "newAirlines" via ASP.NET project with controller

---
+++ WebApplication1/Models/AirlinesContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AirlinesContext : DbContext
+    {
+        public DbSet<Stanowisko> Stanowiska { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+ 
+}
---
+++ WebApplication1/Models/Stanowisko.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    
+    public class Stanowisko
+    {
+        public int StanowiskoId { get; set; }
+        public string Name { get; set; }
+    }
+}
